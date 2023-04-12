e0e235812040d3202d107a9a6681202cda524112
Cyro Sabóia <cyrosaboia@gmail.com>
2023-04-12 09:02:03 -0300

    corrigindo send_mail, porem tem erro no smtp do google

--- app/__init__.py
+++ app/__init__.py
@@ -8,13 +8,13 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 # app.config['SQLALCHEMY_ECHO'] = True
 
 # Configurações de Email # 
-mail = Mail(app)
 app.config['MAIL_SERVER']='smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
 app.config['MAIL_USERNAME'] = 'urbanpass2@gmail.com'
 app.config['MAIL_PASSWORD'] = 'iL17504678@'
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
+mail = Mail(app)
 
 
 login_manager = LoginManager(app)
--- app/controller/send_emailController.py
+++ app/controller/send_emailController.py
@@ -1,10 +1,7 @@
-from flask import flash, render_template, request, redirect, url_for
+from flask import flash, request, redirect, url_for
 from flask_mail import Mail, Message
 from ..rotas.send_emailRout import send_email_bp
-
-
-
-
+from app import mail
 
 class Contato:
         def __init__(self, name, phone, message):
@@ -22,16 +19,17 @@ class Contato:
                                 request.form['phone'],
                                 request.form['message']
                         )
-
-                # Cria uma mensagem de e-mail
-                msg = Message(
-                        subject = f'{formContato.name} enviou uma mensagem.',
-                        sender='urbanpass2@gmail.com',
-                        recipients=['urbanpass2@gmail.com'],
-                        body = f'Nome: {formContato.name}\nTelefone: {formContato.phone}\n Mensagem: {formContato.message}'
+               
+                with mail.connect() as conn:
+                        msg = Message(
+                                subject = f'{formContato.name} enviou uma mensagem.',
+                                sender='urbanpass2@gmail.com',
+                                recipients=['urbanpass2@gmail.com'],
+                                body = f'Nome: {formContato.name}\nTelefone: {formContato.phone}\n Mensagem: {formContato.message}'
                 )
-                
+                        conn.send(msg)
+
                 # Envia a mensagem de e-mail
-                Mail.send(msg)
+                # Mail.send(msg)
                 flash('Obrigado pelo seu contato! Sua mensagem foi enviada com sucesso.')
                 return redirect(url_for('public.cidadao'))
