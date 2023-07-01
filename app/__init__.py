7c38262f051d4db706285c5ffb7a25c23a83debb
Cyro Sabóia <cyrosaboia@gmail.com>
2023-07-01 09:50:53 -0300

    adicionando app ao mapeamento do static

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,7 @@ from flask_mail import Mail
 from whitenoise import WhiteNoise
 
 app = Flask(__name__)
-app.wsgi_app = WhiteNoise(app.wsgi_app, root='static/', prefix='static/')
+app.wsgi_app = WhiteNoise(app.wsgi_app, root='app/static/', prefix='static/')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 #app.debug = True
 # app.config['SQLALCHEMY_ECHO'] = True
